b8df18749fa1c1cc79ffcd5945afb1d768c762c3
JanBergsma <bergsma_jan@hotmail.com>
2025-05-11 11:17:01 +0200

    longestCommonPrefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'vitest'
+
+function longestCommonPrefix(ss) {
+  const minLength = ss.reduce(
+    (acc, val) => Math.min(acc, val.length),
+    ss ? ss[0].length : 0
+  )
+
+  let prefix = ''
+  for (let i = 0; i < minLength; ++i) {
+    const ch = ss[0][i]
+    for (const s of ss) {
+      if (s[i] !== ch) {
+        return prefix
+      }
+    }
+    prefix += ch
+  }
+
+  return prefix
+}
+
+describe('getUniqueCharacters', () => {
+  test('[""] should return ""', () => {
+    expect(longestCommonPrefix([''])).toBe('')
+  })
+  test('["flower","flow","flight"] should return "fl"', () => {
+    expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl')
+  })
+  test('["dog","racecar","car"] should return ""', () => {
+    expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toBe('')
+  })
+})
